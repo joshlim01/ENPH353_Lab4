25bc3dafeea22a2b7cd5cb9460f1bc1a2f50370d
fizzer <joshlim01@gmail.com>
2023-02-08 00:55:24 -0800

    finished SIFT_app

--- SIFT_app.py
+++ SIFT_app.py
@@ -53,6 +53,41 @@ class My_App(QtWidgets.QMainWindow):
 
 	def SLOT_query_camera(self):
 		ret, frame = self._camera_device.read()
+
+		# SIFT and Homography Tracking
+		grayScale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		sift = cv2.SIFT_create()
+		template_img = cv2.imread(self.template_path, 0)
+		
+		kp_image, desc_image = sift.detectAndCompute(template_img, None)
+		kp_camera, desc_camera = sift.detectAndCompute(grayScale, None)
+
+		index_params = dict(algorithm=0, trees=5)
+		search_params = dict()
+
+		flann = cv2.FlannBasedMatcher(index_params, search_params)
+		matches = flann.knnMatch(desc_image, desc_camera, k=2)
+		good_points = []
+		
+		for m, n in matches:
+			if (m.distance < 0.6 * n.distance):
+				good_points.append(m)
+
+		if(len(good_points) >= 4):
+			query_pts = np.float32([kp_image[m.queryIdx].pt for m in good_points]).reshape(-1, 1, 2)
+			train_pts = np.float32([kp_camera[m.trainIdx].pt for m in good_points]).reshape(-1, 1, 2)
+
+			matrix, mask = cv2.findHomography(query_pts, train_pts, cv2.RANSAC, 5.0)
+			matches_mask = mask.ravel().tolist()
+
+			# Perspective transform
+			h, w = template_img.shape
+			pts = np.float32([[0, 0], [0, h], [w, h], [w, 0]]).reshape(-1, 1, 2)
+			dst = cv2.perspectiveTransform(pts, matrix)
+
+			homography = cv2.polylines(frame, [np.int32(dst)], True, (255, 0, 0), 3)
+			cv2.imshow("Homography", homography)
+
 		#TODO run SIFT on the captured frame
 
 		pixmap = self.convert_cv_to_pixmap(frame)
